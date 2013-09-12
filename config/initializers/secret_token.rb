b7fd12052160248d63817e06d5b9cca0631a47d3
Andrew DuBrock <andrewdu@Andrew-DuBrocks-Computer-2.local>
2013-09-11 21:57:28 -0700

    Created a 'Hello World' Program with Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hello::Application.config.secret_token = 'a3587425d4948c74e3118f6198bf0d6d41e6f6dc030770343c45f3c225866710b38921aab03e2b177456b653e78ac08d98ed874732c7284bceec7320d79879fd'
